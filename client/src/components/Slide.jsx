32c35e4155976c628846893254053dce0ade303d
divyanshu <divyanshupal249@gmail.com>
2025-10-30 09:01:45 +0530

    first commit

---
+++ client/src/components/Slide.jsx
@@ -0,0 +1,14 @@
+import "../styles/Slide.scss"
+
+const Slide = () => {
+  return (
+    <div className="slide">
+      <h1>
+       Welcome back! Wherever life takes you <br /> live in the moment and create lasting memories.
+        memories
+      </h1>
+    </div>
+  );
+};
+
+export default Slide;
